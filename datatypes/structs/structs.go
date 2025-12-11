418d9f707f0593d9ff90a866f1c7c32cc1603715
Abhishek <abhi.anandwade@gmail.com>
2025-12-11 16:12:41 +0530

    Refactor CreateStudent to accept Car parameter and update student struct; enhance UpdateStudentMarks with logging

--- datatypes/structs/structs.go
+++ datatypes/structs/structs.go
@@ -13,16 +13,34 @@ type student struct {
 	City      string
 	Email     string
 	Marks     float64
+	Cars      Car
 }
 
-func CreateStudent(firstName, lastName string, age int, city, email string, marks float64) student {
-	return student{
+func CreateStudent(firstName, lastName string, age int, city,
+	email string, marks float64, cars Car) *student {
+	newStudent := student{
 		FirstName: firstName,
 		LastName:  lastName,
 		Age:       age,
 		City:      city,
 		Email:     email,
 		Marks:     marks,
+		Cars:      cars,
+	}
+	return &newStudent
+}
+
+type Car struct {
+	Name  string
+	Model string
+	Year  int
+}
+
+func CreateCar(name, model string, year int) Car {
+	return Car{
+		Name:  name,
+		Model: model,
+		Year:  year,
 	}
 }
 
@@ -32,8 +50,19 @@ func CreateStudents(s student, r student) []student {
 	return newSlice
 }
 
-func UpdateStudentMarks(s student, newMarks float64) {
+func UpdateStudentMarks(s *student, newMarks float64) {
 	s.Marks = newMarks
+	fmt.Println("Updated Marks inside function:", s.Marks)
+}
+
+func (s *student) UpdateStudentMarks(newMarks float64) {
+	s.Marks = newMarks
+	fmt.Println("Updated Marks inside function:", s.Marks)
+}
+
+func (s *student) GetStudentName() {
+	fmt.Println("First Name:", s.FirstName)
+	fmt.Println("Last Name:", s.LastName)
 }
 
 func GetStudentDetails(s student) {
@@ -48,14 +77,28 @@ func GetStudentDetails(s student) {
 }
 
 func StudentDemo() {
-	var s student = student{
-		FirstName: "Ram",
-		LastName:  "Sharma",
-		Age:       20,
-		City:      "New York",
-		Email:     "ram@example.com",
-		Marks:     85.5,
-	}
+	// var s student = student{
+	// 	FirstName: "Ram",
+	// 	LastName:  "Sharma",
+	// 	Age:       20,
+	// 	City:      "New York",
+	// 	Email:     "ram@example.com",
+	// 	Marks:     85.5,
+	// }
+
+	c := CreateCar("Toyota", "Camry", 2020)
+
+	r := CreateStudent("Sita", "Verma", 22, "Los Angeles",
+		"sita@example.com", 90.0, c)
+
+	// s := CreateStudent("Ram", "Sharma", 20, "New York",
+	// 	"example.com", 90.0, c)
+
+	UpdateStudentMarks(r, 95.0)
+	r.UpdateStudentMarks(97.0)
+	r.GetStudentName()
+
+	fmt.Println("Marks after UpdateStudentMarks call:", r.Cars.Name)
 
 	//  r := student{
 	// 	FirstName: "Ram",
@@ -66,17 +109,17 @@ func StudentDemo() {
 	// 	Marks:     85.5,
 	// }
 
-	var r student
-	r.FirstName = "Sita"
-	r.LastName = "Verma"
-	r.Age = 22
-	r.City = "Los Angeles"
-	r.Email = "sita@example.com"
-	r.Marks = 90.0
+	// var r student
+	// r.FirstName = "Sita"
+	// r.LastName = "Verma"
+	// r.Age = 22
+	// r.City = "Los Angeles"
+	// r.Email = "sita@example.com"
+	// r.Marks = 90.0
 
-	// var s1 student = student{"Ram", "Sharma", 20, "New York", "ram@example.com", 85.5}
-	// fmt.Println("Empty Student Struct:", s1)
-	fmt.Println(CreateStudents(s, r))
+	// // var s1 student = student{"Ram", "Sharma", 20, "New York", "ram@example.com", 85.5}
+	// // fmt.Println("Empty Student Struct:", s1)
+	// fmt.Println(CreateStudents(s, r))
 	// s1 = CreateStudent("Ram", "Sharma", 20, "New York", "ram@example.com", 85.5)
 	// s1.GetStudentDetails()
 	// s1.UpdateMarks(90.0)
